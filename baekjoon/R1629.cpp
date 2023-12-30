72ad905f6462c78165d88cfaf49e9b219c520972
H2ll0World <104723794+H2ll0World@users.noreply.github.com>
2023-12-30 21:33:15 +0900

    Create R1629.cpp

---
+++ baekjoon/R1629.cpp
@@ -0,0 +1,24 @@
+/*
+	1629 곱셈
+*/
+
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+ll pow(ll a, ll b, ll c) {
+	if (b == 1) return a % c;
+	ll val = pow(a, b / 2, c);
+	val = val * val % c;
+	if (b % 2 == 0) return val % c;
+	return val * a % c;
+}
+
+int main() {
+	long long A, B, C, result = 1;
+	cin >> A >> B >> C;
+	
+	cout << pow(A, B, C);
+
+	return 0;
+}
