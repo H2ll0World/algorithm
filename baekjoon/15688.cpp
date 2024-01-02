f6acc3a02ee2d19be8b7a11906c8af11dbedfd9f
H2ll0World <104723794+H2ll0World@users.noreply.github.com>
2024-01-02 18:45:49 +0900

    Create 15688.cpp

---
+++ baekjoon/15688.cpp
@@ -0,0 +1,27 @@
+// 15688
+
+#include <iostream>
+using namespace std;
+
+int arr[2000001];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    int temp;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> temp;
+        arr[temp + 1000000]++;
+    }
+
+    for(int i = 0; i < 2000001; i++){
+        while(arr[i]-- > 0){
+            cout << i - 1000000 << '\n';
+        }
+    }
+
+    return 0;
+}
